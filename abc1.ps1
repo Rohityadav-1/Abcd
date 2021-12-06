32216c1d234f8583782237fbbb55a139087f3a27
Rohit Kumar Yadav <rohit.k.yadav@intimetec.com>
2021-12-06 14:44:54 +0530

    this is my first commit

---
+++ abc1.ps1
@@ -0,0 +1,20 @@
+﻿function leapYear {
+    param([int] $input_year)
+    process
+    {
+        try {
+              if ( $input_year % 4 -eq 0 ) {
+              echo "year is leapyear"
+            }
+            }
+            catch 
+            {
+                     write-output "input/output exception"
+                   }
+            else {
+            echo "year is not a leap_year"
+            }
+    }
+}
+
+     
